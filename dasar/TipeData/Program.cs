8d9a4474c3bf962c6b2d3c11e26d2d0e6ba8862a
Unero <20809454+Unero@users.noreply.github.com>
2020-01-08 19:08:55 +0700

    Tipe Data Topic

---
+++ dasar/TipeData/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TipeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Types in C#
+            string phrase = "Unero";
+            char grade = 'B';
+            int age = 20;
+
+            // Decimal type data
+            // float, double, decimal
+            double gpa = 3.9;
+            bool isMale = false;
+
+            Console.ReadLine();
+        }
+    }
+}
