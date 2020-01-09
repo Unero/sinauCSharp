6985be1c1b88c3bfa43782d8c3ce1914ceba1386
Unero <20809454+Unero@users.noreply.github.com>
2020-01-09 07:14:53 +0700

    Topic: Working with Numbers

---
+++ dasar/WorkingWithNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WorkingWithNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mathematic Operation
+            Console.WriteLine(1 + 1);
+            Console.WriteLine(1 - 1);
+            Console.WriteLine(1 / 1);
+            Console.WriteLine(1 * 1);
+            Console.WriteLine(1 % 1);
+
+            int num = 6;
+            Console.WriteLine(num);
+            Console.WriteLine(num++);
+            Console.WriteLine(num--);
+
+            Console.WriteLine(Math.Abs(-num));
+            Console.WriteLine(Math.Pow(num, 2));
+            Console.WriteLine(Math.Sqrt(Math.Pow(num, 2)));
+            Console.WriteLine(Math.Max(num, 100));
+            Console.WriteLine(Math.Min(num, 100));
+            Console.WriteLine(Math.Round(5.1));
+            Console.WriteLine(Math.PI);
+
+            Console.ReadLine();
+        }
+    }
+}
